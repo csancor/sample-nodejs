1e044e09e436d0835d40e69fc80cacba216c0741
Cleiton dos Santos <coding.cord@outlook.com>
2023-04-16 00:17:09 -0300

    Testes com o Jest

---
+++ __tests__/server.test.js
@@ -0,0 +1,10 @@
+// const request = require('supertest');
+
+
+// describe('Teste de rota GET /', () => {
+//     test('Deve retornar status 200', async () => {
+//       const res = await request("http://localhost:3000").get('/');
+//       expect(res.statusCode).toEqual(200);
+//     });
+//   });
+  
